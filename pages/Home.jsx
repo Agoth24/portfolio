44bb49a9639e355d987d0d5231e81e992618020f
Agoth Arop <agoth.a3@gmail.com>
2026-04-22 01:09:45 -0600

    logo checkpoint

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,6 +1,8 @@
+import logos from "../src/logos";
+
 const Home = () => {
 	return (
-		<section>
+		<section className="flex flex-col gap-8">
 			<div className="flex flex-col gap-4 dark:text-zinc-200 font-space-mono mt-8">
 				<p>
 					&gt; Hey there! I'm Agoth, a Software Engineer and Computer
@@ -14,6 +16,12 @@ const Home = () => {
 					</p>
 				</div>
 			</div>
+            <div className="flex items-center justify-between">
+                {logos.map((logo) => (
+                    <img className="h-10 w-auto" src={logo.src} alt={logo.name} />
+                ))}
+            </div>
+
 		</section>
 	);
 };
---
+++ src/logos.jsx
@@ -0,0 +1,31 @@
+const logos = [
+	{
+		name: "node",
+        src: "../assets/logos/Node.js_logo.svg"
+	},
+	{
+		name: "react",
+        src: "../assets/logos/React-icon.svg"
+	},
+	{
+		name: "ts",
+        src: "../assets/logos/Typescript_logo_2020.svg"
+	},
+	{
+		name: "python",
+        src: "../assets/logos/Python-logo-notext.svg"
+	},
+	// {
+	// 	name: "express",
+    //     src: "../assets/logos/"
+	// },
+	{
+		name: "tailwind",
+        src: "../assets/logos/Tailwind_CSS_Logo.svg"
+	},
+	{
+		name: "next",
+        src: "../assets/logos/Node.js_logo.svg"
+	},
+];
+export default logos;
